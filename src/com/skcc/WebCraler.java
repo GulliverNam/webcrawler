0dd54e67a62d1146211edbe0438c5f4e96ac134e
Giwon <Giwon@121.157.161.14>
2021-04-12 23:16:20 +0900

    마이데이터 IO 스펙 크롤링 완성

--- src/com/skcc/WebCraler.java
+++ src/com/skcc/WebCraler.java
@@ -6,6 +6,10 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -18,15 +22,14 @@ import org.jsoup.select.Elements;
 
 public class WebCraler {
 	private final static String baseUrl = "https://developers.mydatakorea.org/mdtb/apg/mac/bas";
-	private final static String[] params = {"/FSAG0404?id=1"};
-//	,
-//										    "/FSAG0406?id=2",
-//										    "/FSAG0403?id=3",
-//										    "/FSAG0402?id=4",
-//										    "/FSAG0405?id=5",
-//										    "/FSAG0407?id=6",
-//										    "/FSAG0408?id=10",
-//										    "/FSAG0409?id=11"};
+	private final static String[] params = {"/FSAG0404?id=1",
+										    "/FSAG0406?id=2",
+										    "/FSAG0403?id=3",
+										    "/FSAG0402?id=4",
+										    "/FSAG0405?id=5",
+										    "/FSAG0407?id=6",
+										    "/FSAG0408?id=10",
+										    "/FSAG0409?id=11"};
 	public static void main(String[] args) throws Exception {
 		try {
 			ConnectionUtils.ignoreSSL();
@@ -53,11 +56,59 @@ public class WebCraler {
 	        in.close();
 	        conn.disconnect();
 	        Document jsoup = Jsoup.parse(response.toString());
-	        Elements apis = jsoup.getElementsByClass("api_section_list");
+	        Elements apis = jsoup.getElementsByClass("api_section_list").get(0).children();
 	        int size = apis.size();
+	        System.out.println("size: "+ size);
 	        for(int i=0; i<size; i++) {
 	        	Element api = apis.get(i);
-//	        	Element head = api.getElementsByClass("board_list left").;
+	        	Elements head = api.getElementsByClass("board_list left").get(0).getElementsByTag("tbody").get(0).getElementsByTag("tr");
+	        	String apiName = head.get(2).getElementsByTag("td").get(0).text();
+	        	String apiDesc = head.get(3).getElementsByTag("td").get(0).text();
+	        	System.out.println(apiName+" "+apiDesc);
+	        	System.out.println("###########################################");
+	        	Elements bodies = api.getElementsByClass("board_list row_line");
+	        	Elements body1 = bodies.get(0).getElementsByTag("tbody").get(0).getElementsByTag("tr");
+	        	int bodySize = body1.size();
+	        	int paramIdx = 1;
+	        	int descIdx = 2;
+	        	int typeIdx = 4;
+	        	boolean flag = false;
+	        	List<String[]> reqList = new ArrayList<>();
+	        	for (int j = 0; j < bodySize; j++) {
+	        		Elements td = body1.get(j).getElementsByTag("td");
+	        		if(flag) {
+	        			reqList.add(new String[] {td.get(paramIdx).text(), td.get(descIdx).text(), td.get(typeIdx).text()});
+	        		}else if(td.get(0).text().equals("Parameter") || td.get(0).text().equals("Body")) {
+	        			reqList.add(new String[] {td.get(paramIdx).text(), td.get(descIdx).text(), td.get(typeIdx).text()});
+						paramIdx--;
+						descIdx--;
+						typeIdx--;
+						flag = true;
+					}
+				}
+	        	for (String[] arr : reqList) {
+					System.out.println(arr[0]+" "+arr[1]+" "+arr[2]);
+				}
+	        	System.out.println("###########################################");
+	        	Elements body2 = bodies.get(1).getElementsByTag("tbody").get(0).getElementsByTag("tr");
+	        	bodySize = body2.size();
+	        	paramIdx = 1;
+	        	descIdx = 2;
+	        	typeIdx = 4;
+	        	List<String[]> respList = new ArrayList<>();
+	        	for (int j = 0; j < bodySize; j++) {
+	        		Elements td = body2.get(j).getElementsByTag("td");
+	        		respList.add(new String[] {td.get(paramIdx).text(), td.get(descIdx).text(), td.get(typeIdx).text()});
+	        		if(j == 0) {
+						paramIdx--;
+						descIdx--;
+						typeIdx--;
+					}
+				}
+	        	for (String[] arr : respList) {
+					System.out.println(arr[0]+" "+arr[1]+" "+arr[2]);
+				}
+	        	System.out.println("###########################################");
 	        }
 			
 		}
