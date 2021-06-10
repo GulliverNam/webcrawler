a93a85dc25f759abf6eb0707039e5fa2278f034f
GiwonNam <gullivernam94@gmail.com>
2021-06-11 00:07:18 +0900

    210621 mydata 모든 api Request & Response 데이터 정의

--- src/com/skcc/MakeRequestSet.java
+++ src/com/skcc/MakeRequestSet.java
@@ -38,10 +38,15 @@ public class MakeRequestSet {
 										    "/FSAG0302?id=9", 	// 지원(사업자/정보제공자 제공)
 										    "/FSAG0301?id=8" 	// 지원(종합포털제공)
 //										    "/FSAG0201?id=7" 	// 인증
-										    }; 
+										    };
+	private static int rowSize = 0;
+	
 	public static void main(String[] args) throws Exception {
 		ConnectionUtils.ignoreSSL();
 		URL url = null;
+		String filePath = ".\\data_req\\result.xlsx";
+    	XSSFWorkbook workbook = new XSSFWorkbook();
+    	XSSFSheet sheet = workbook.createSheet("IO_LIST");
 		for(String param : params) {
 			url = new URL(baseUrl+param);
 			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
@@ -65,19 +70,11 @@ public class MakeRequestSet {
 	        int size = apis.size();
 	        System.out.println("size: "+ size);
 	        
-	        String originFilePath = ".\\data_req\\sample.xlsx";
-	        File originFile = new File(originFilePath);
+	        
 	        for(int i=0; i<size; i++) {
 	        	Element api = apis.get(i);
 	        	Elements head = api.getElementsByClass("board_list left").get(0).getElementsByTag("tbody").get(0).getElementsByTag("tr");
 	        	String[] apiName = head.get(2).getElementsByTag("td").get(0).text().split("/");
-	        	String apiDesc = head.get(3).getElementsByTag("td").get(0).text();
-	        	String copyfilePath = ".\\data_req\\"+String.join("_", apiName)+".xlsx";
-	        	File copyFile = new File(copyfilePath);
-	        	if(!copyFile.exists()) {
-	        		Files.copy(originFile.toPath(), copyFile.toPath());
-	        	}
-	        	System.out.println(copyfilePath);
 	        	System.out.println("###########################################");
 	        	Elements table = api.getElementsByClass("board_list row_line");
 	        	Elements requestData = table.get(0).getElementsByTag("tbody").get(0).getElementsByTag("tr");
@@ -112,7 +109,7 @@ public class MakeRequestSet {
 				}
 	        	
 	        	
-	        	createData(copyfilePath, reqList, "I", 2);
+	        	createData(sheet, reqList, "I");
 	        	System.out.println("###########################################");
 	        	Elements responseData = table.get(1).getElementsByTag("tbody").get(0).getElementsByTag("tr");
 	        	bodySize = responseData.size();
@@ -137,7 +134,7 @@ public class MakeRequestSet {
 	        	for (API temp : respList) {
 					System.out.println(temp);
 				}
-	        	createData(copyfilePath, respList, "O", 2+reqList.size());
+	        	createData(sheet, respList, "O");
 	        	System.out.println("###########################################");
 	        }
 			
@@ -145,78 +142,85 @@ public class MakeRequestSet {
 		FileOutputStream fos = new FileOutputStream(filePath);
     	workbook.write(fos);
     	fos.close();
-    	fis.close();
 	}
-	private static void createData(List<API> List, String IO, int rowSize) throws IOException, InvalidFormatException {
-		
-		FileInputStream fis = new FileInputStream(filePath);
-    	XSSFWorkbook workbook = new XSSFWorkbook(fis);
-    	XSSFSheet sheet = workbook.getSheet("IO_LIST");
-    	
+	private static void createData(XSSFSheet sheet, List<API> list, String IO) throws IOException, InvalidFormatException {
     	XSSFRow row;
     	String parentId = "";
-    	for(int i = 0, listSize = List.size(); i < listSize; i++) {
-    		API api = List.get(i);
-    		row = sheet.createRow(i+rowSize);
+    	for(int i = 0, listSize = list.size(); i < listSize; i++) {
+    		API api = list.get(i);
+    		row = sheet.createRow(rowSize++);
     		// 0. API ID
     		row.createCell(0).setCellValue(api.getApiId());
     		// 1. 입력/출력(I,O) & 헤드/바디(H,B)
-    		String headBody = "";
+    		String IOheadBody = "";
     		if("Header".equals(api.getHeaderBody())) {
-    			IO += "H";
-    		} else {
-    			IO += "B";
+    			IOheadBody = IO + "H";
+    		}else if("Path".equals(api.getHeaderBody())){
+    			IOheadBody = IO + "P";
+    		}else {
+    			IOheadBody = IO + "B";
     		}
-    		row.createCell(1).setCellValue(IO);
-    		String id = api[0].replaceAll("-", "");
+    		row.createCell(1).setCellValue(IOheadBody);
     		String[] UIOType = null;
     		String referLength = "";
-    		// 2. 상위항목
-    		row.createCell(2).setCellValue(parentId);
-    		// 1. 항목유형(F,R)
-			if(api[2].toLowerCase().contains("object")) {
-    			row.createCell(1).setCellValue("R");
+    		if(!"".equals(parentId)) {
+    			api.setContentId(api.getContentId().replaceAll("-", ""));
+    		}
+    		// 3. 상위항목
+    		row.createCell(3).setCellValue(parentId);
+    		// 2. 항목유형(F,R)
+			if(api.getTypeLength().toLowerCase().contains("object")) {
+    			row.createCell(2).setCellValue("R");
     			UIOType = new String[] {"", ""};
     			if(!"".equals(parentId)) {
     				parentId += ".";
     			}
-    			parentId += api[0].replaceAll("-", "");
-    			referLength = List.get(i-1)[0].replaceAll("-", "");
+    			parentId += api.getContentId();
+    			API refer = list.get(i-1);
+    			referLength = list.get(i-1).getContentId();
 			} else {
-				row.createCell(1).setCellValue("F");
+				row.createCell(2).setCellValue("F");
 			}
 			
 			String checkLogic = "";
-			if(id.contains("timestamp") || api[2].toLowerCase().contains("dtime")) {
+			if(api.getContentId().contains("timestamp") || api.getTypeLength().toLowerCase().contains("dtime")) {
 				UIOType = new String[] {"string", "14"};
 				checkLogic = "timestamp14_check";
-			} else if(api[2].toLowerCase().contains("date")) {
+			} else if(api.getTypeLength().toLowerCase().contains("date")) {
 				UIOType = new String[] {"string", "8"};
 				checkLogic = "date_check";
-			} else if(api[2].toLowerCase().contains("boolean")) {
+			} else if(api.getTypeLength().toLowerCase().contains("boolean")) {
 				UIOType = new String[] {"string", "5"};
 				checkLogic = "boolean_check";
+			} else if("Path".equals(api.getHeaderBody())) {
+				UIOType = new String[] {"string", "10"};
+				if("".equals(api.getDescription())) {
+					api.setDescription("Path 변수");
+				}
 			} else if(UIOType == null) {
-				UIOType = getUIOType(api[2]);
+				UIOType = getUIOType(api.getTypeLength());
 			}
 			
-			// 3. 항목
-			row.createCell(3).setCellValue(id);
-			// 4. 항목명
-			row.createCell(4).setCellValue(api[1]);
-			// 5. 길이
-			row.createCell(5).setCellValue(UIOType[1]);
-			// 6. 길이참조(RecordSet 일때)
-			row.createCell(6).setCellValue(referLength);
-			// 7. 데이터유형
-			row.createCell(7).setCellValue(UIOType[0]);
+			// 4. 항목
+			row.createCell(4).setCellValue(api.getContentId());
+			// 5. 항목명
+			row.createCell(5).setCellValue(api.getContentName());
+			// 6. 길이
+			row.createCell(6).setCellValue(UIOType[1]);
+			// 7. 길이참조(RecordSet 일때)
+			row.createCell(7).setCellValue(referLength);
+			// 8. 데이터유형
+			row.createCell(8).setCellValue(UIOType[0]);
 			
-			// 10. 필수검증 (Y/N)
-			if(api[3].equals("Y")) {
-				row.createCell(10).setCellValue("mandatory");
+			// 11. 필수검증 (Y/N)
+			if(api.isRequired()) {
+				row.createCell(11).setCellValue("mandatory");
 			}
-			// 11. 검증로직 (timestamp14_check, date_check, boolean_check)
-			row.createCell(11).setCellValue(checkLogic);
+			// 12. 검증로직 (timestamp14_check, date_check, boolean_check)
+			row.createCell(12).setCellValue(checkLogic);
+			
+			// 13. 설명
+			row.createCell(13).setCellValue(api.getDescription());
     	}
     	
 	}
